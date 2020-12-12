304dffdd56e1ce8ad9693f79a3a79406193e536a
Evgenii Markov <emarkov@emarkov-mn1.linkedin.biz>
2020-12-12 12:13:02 -0800

    set webpack with plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: './src/app.js',
   output: {
@@ -8,4 +10,10 @@ module.exports = {
   devServer: {
     port: 3000,
   },
+  plugins: [
+    new HTMLPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
