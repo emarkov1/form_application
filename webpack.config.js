4447c2f90e41877dcbf2c91fc2e78a4c722d595e
Evgenii Markov <emarkov@emarkov-mn1.linkedin.biz>
2020-12-12 12:03:53 -0800

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.[chunkhash].js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    port: 3000,
+  },
+};
